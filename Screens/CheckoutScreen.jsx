c2b14bd9d42ea7ab936a6e2e8822a8dcb508f709
thedevilx <aadithya794@gmail.com>
2020-10-22 02:50:34 +0530

    added Ethereum

--- Screens/CheckoutScreen.jsx
+++ Screens/CheckoutScreen.jsx
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
-import {View, StatusBar, StyleSheet, Text, ScrollView} from 'react-native';
+import React, { useState } from 'react';
+import { View, StatusBar, StyleSheet, Text, ScrollView, Linking } from 'react-native';
 import theme from '../theme.json';
-import {SvgUri} from 'react-native-svg';
+import { SvgUri } from 'react-native-svg';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
+import Axios from 'axios'
 import {
   TextInput,
   TouchableWithoutFeedback,
 } from 'react-native-gesture-handler';
 import DateTimePicker from '@react-native-community/datetimepicker';
-function CheckoutScreen({route,navigation}) {
+function CheckoutScreen({ route, navigation }) {
   const [date, setDate] = useState(new Date(1598051730000));
   const [show, setShow] = useState(false);
   const [satelliteName, setSatelliteName] = useState("");
@@ -21,19 +22,8 @@ function CheckoutScreen({route,navigation}) {
     setDate(selDate);
   };
 
-  const launch = async() => {
-      if(satelliteName.length>0){
-          await firestore().collection("satellites").add({
-              isOnline: false,
-              data: [],
-              createdBy: auth().currentUser.uid,
-              name: satelliteName
-          }).then(() => {
-              setSatelliteName("")
-              navigation.pop()
-              navigation.navigate("HomeScreen")
-          })
-      }
+  const launch = async () => {
+    Linking.openURL("https://commerce.coinbase.com/checkout/418b401d-abef-4673-917a-967f4f4ab3b9")
   }
 
   return (
@@ -77,8 +67,8 @@ function CheckoutScreen({route,navigation}) {
             fill={theme.primary}></SvgUri>
           <TextInput
             placeholder="Satellite Name"
-            value = {satelliteName}
-            onChangeText = {(e) => setSatelliteName(e)}
+            value={satelliteName}
+            onChangeText={(e) => setSatelliteName(e)}
             style={Styles.textInput}></TextInput>
         </View>
         <View style={Styles.inputContainer}>
@@ -108,7 +98,7 @@ function CheckoutScreen({route,navigation}) {
             uri="https://raw.githubusercontent.com/cchirag/GitMath/master/Dollar.svg"
             fill={theme.primary}></SvgUri>
           <Text style={Styles.launchDateText} onPress={() => setShow(true)}>
-                Total: ${route.params.total.toFixed(2)}
+            Total: ${route.params.total.toFixed(2)}
           </Text>
         </View>
       </View>
